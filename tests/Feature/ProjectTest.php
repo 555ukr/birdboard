d3b461c319340b6004a48c4cc97e76e957275fec
Dmytro Kalashnyk <d.kalashnyk@Administrators-MacBook-Pro.local>
2020-12-16 15:21:53 +0100

    first day

---
+++ tests/Feature/ProjectTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ProjectTest extends TestCase
+{
+    use WithFaker, RefreshDatabase;
+    /**
+     * @test
+     */
+    public function a_user_can_create_a_project()
+    {    
+        $this->withoutExceptionHandling();
+        
+        $attributes = [
+            'title' => $this->faker->sentence,
+            'description' => $this->faker->paragraph
+        ];
+
+        $this->post('/projects', $attributes)->assertRedirect('/projects');
+
+        $this->assertDatabasehas('projects', $attributes);
+
+        $this->get('/projects')->assertSee($attributes['title']);
+    }
+
+
+    /** @test */
+    public function a_project_requires_a_title()
+    {
+        $this->post('/projects', [])->assertSessionHasErrors('title');
+    }
+
+    /** @test */
+    public function a_project_requires_a_description()
+    {
+        $this->post('/projects', [])->assertSessionHasErrors('description');
+    }
+}
